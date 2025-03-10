b1b1f05236ade2916f00101a722882740f0aefc8
Ria Thomas <ria.thomas@morsemicro.com>
2025-03-10 18:24:07 +1100

    mac80211: IBSS bridge support
    With the addition of BATMAN routing protocol, an IBSS (ad hoc) interface
    can be bridged with Ethernet intefaces. Add bridge mode support for IBSS
    interfaces.
    
    Morse Micro note: This commit is required if IBSS is used.

--- net/wireless/core.c
+++ net/wireless/core.c
@@ -1113,8 +1113,8 @@ static int cfg80211_netdev_notifier_call(struct notifier_block *nb,
 		wdev->ps_timeout = -1;
 
 		if ((wdev->iftype == NL80211_IFTYPE_STATION ||
-		     wdev->iftype == NL80211_IFTYPE_P2P_CLIENT ||
-		     wdev->iftype == NL80211_IFTYPE_ADHOC) && !wdev->use_4addr)
+		     wdev->iftype == NL80211_IFTYPE_P2P_CLIENT) &&
+            !wdev->use_4addr)
 			dev->priv_flags |= IFF_DONT_BRIDGE;
 
 		nl80211_notify_iface(rdev, wdev, NL80211_CMD_NEW_INTERFACE);
