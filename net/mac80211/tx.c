MorseMicro/linux

mac80211: tx s1g AP ecsa support
Update the AP-side MLME to consider short beacons in channel switch time
calculations, which includes an ECSA IE only in DTIM beacons. ECSA countdown
is updated only for DTIM beacons and not for short beacons.

This patch is S1G-specific and can have an impact on ECSA functionality of
other Wi-Fi radios, which delays channel switch operation based on the DTIM
interval. Impact on the ECSA functionality can be mitigated when combined with
'mac80211: mlme s1g ecsa support' and used on both APs and stations.

net/mac80211/tx.c
@@ -4096,7 +4096,9 @@ static void ieee80211_set_csa(struct ieee80211_sub_if_data *sdata,
 
 static u8 __ieee80211_csa_update_counter(struct beacon_data *beacon)
 {
-	beacon->csa_current_counter--;
+	/* Avoid updating once the count down is complete */
+	if (beacon->csa_current_counter > 1)
+		beacon->csa_current_counter--;
 
 	/* the counter should never reach 0 */
 	WARN_ON_ONCE(!beacon->csa_current_counter);
@@ -4242,10 +4244,15 @@ __ieee80211_beacon_get(struct ieee80211_hw *hw,
 
 	if (sdata->vif.type == NL80211_IFTYPE_AP) {
 		struct ieee80211_if_ap *ap = &sdata->u.ap;
+		bool short_beacon = (vif->bss_conf.dtim_period > 1);
+
+		if (ap->ps.dtim_count > 0)
+			short_beacon = ((ap->ps.dtim_count-1) != 0);
 
 		beacon = rcu_dereference(ap->beacon);
 		if (beacon) {
-			if (beacon->csa_counter_offsets[0]) {
+			/* Do not count channel switch count for short beacons */
+			if (beacon->csa_counter_offsets[0] && !short_beacon) {
 				if (!is_template)
 					__ieee80211_csa_update_counter(beacon);
 

MorseMicro/rpi-linux

net/mac80211/tx.c
@@ -5032,10 +5032,10 @@ static void ieee80211_set_beacon_cntdwn(struct ieee80211_sub_if_data *sdata,
 
 static u8 __ieee80211_beacon_update_cntdwn(struct beacon_data *beacon)
 {
-	beacon->cntdwn_current_counter--;
-
 	/* the counter should never reach 0 */
 	WARN_ON_ONCE(!beacon->cntdwn_current_counter);
+
+	beacon->cntdwn_current_counter--;
 
 	return beacon->cntdwn_current_counter;
 }
@@ -5306,7 +5306,13 @@ ieee80211_beacon_get_ap(struct ieee80211_hw *hw,
 	u16 csa_off_base = 0;
 	int mbssid_len;
 
-	if (beacon->cntdwn_counter_offsets[0]) {
+	bool short_beacon = (vif->bss_conf.dtim_period > 1);
+
+	if (ap->ps.dtim_count > 0)
+		short_beacon = ((ap->ps.dtim_count-1) != 0);
+
+	 /* Do not count channel switch count for short beacons */
+	if (beacon->cntdwn_counter_offsets[0] && !short_beacon) {
 		if (!is_template)
 			ieee80211_beacon_update_cntdwn(vif, link->link_id);
 